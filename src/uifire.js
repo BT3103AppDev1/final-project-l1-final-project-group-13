871bb2848b7253e0c3648d565fd9d46698d55ceb
SPACEYNESS909 <brandonlsl010911@gmail.com>
2023-10-21 16:25:31 +0800

    demo

---
+++ src/uifire.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDMjVsdSnxQU5ePQzpW1zzrXdlhEAAlt_w",
+    authDomain: "studyhive-267da.firebaseapp.com",
+    projectId: "studyhive-267da",
+    storageBucket: "studyhive-267da.appspot.com",
+    messagingSenderId: "620905783449",
+    appId: "1:620905783449:web:23f6d505aaaa4774975808",
+    measurementId: "G-GHNMPC1P45"
+  };
+
+  
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+    <div style = "text-align: center;" v-if="user">
+    <h3> This is About Page</h3>
+    <h2> Welcome to CPP</h2>
+    <h3> Crypto Paper Portfolio (CPP) is an app to track your crypto portfolio</h3>
+    </div>
+</template>
+
+<script>
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+
+export default {
+    name: "About",
+    data() {
+        return {
+            user: false
+        }
+    },
+
+    mounted() {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                this.user = user;
+            }
+        })
+    }
+ }
+</script>
