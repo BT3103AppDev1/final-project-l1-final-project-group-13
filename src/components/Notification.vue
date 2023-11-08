b50a333a1184c39206c327924e2f9ce2673409bb
SPACEYNESS909 <brandonlsl010911@gmail.com>
2023-11-09 00:56:27 +0800

    made sidebar sticky

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,26 +1,34 @@
 <template>
-  <span class="material-icons" id="bell-icon" @click="toggleNotification"
-    >notifications</span
-  >
-  <div :class="['notification-bar', { show: showNotification }]">
-    <div id="header" class="header-container">
-      <h3 class="notification-header">Notifications</h3>
-      <span class="material-icons" id="close-icon" @click="toggleNotification"
-        >close</span
-      >
-      <br />
-    </div>
-    <div
-      v-for="notification in notifications"
-      :key="notification.id"
-      class="notification-item"
+  <div class="notification-container">
+    <span
+      class="material-icons"
+      id="bell-icon"
+      @click="toggleNotification"
+      >notifications</span
     >
-      <div id="info">
-        <span class="material-symbols-outlined" id="info-icon">info</span>
+    <div :class="['notification-bar', { show: showNotification }]">
+      <div id="header" class="header-container">
+        <h3 class="notification-header">Notifications</h3>
+        <span
+          class="material-icons"
+          id="close-icon"
+          @click="toggleNotification"
+          >close</span
+        >
+        <br />
       </div>
-      <div id="noti">
-        <h5>{{ notification.title }}</h5>
-        <h6>{{ notification.time }}</h6>
+      <div
+        v-for="notification in notifications"
+        :key="notification.id"
+        class="notification-item"
+      >
+        <div id="info">
+          <span class="material-symbols-outlined" id="info-icon">info</span>
+        </div>
+        <div id="noti">
+          <h5>{{ notification.title }}</h5>
+          <h6>{{ notification.time }}</h6>
+        </div>
       </div>
     </div>
   </div>
@@ -93,10 +101,11 @@ export default {
 .notification-container {
   position: relative;
   display: flex;
+  z-index: 999; /* Set a high z-index value to ensure the notification bar appears in front of the page content */
 }
 
 .notification-bar {
-  position: absolute;
+  position: fixed;
   top: 0;
   right: 0;
   height: 100%;
@@ -105,8 +114,8 @@ export default {
   overflow-y: auto;
   transition: transform 0.3s ease-in-out;
   transform: translateX(100%);
+  z-index: 1000; /* Set a z-index higher than the page content */
 }
-
 #bell-icon {
   font-size: 2rem;
   color: #000000;
--- src/views/UserProfilePage.vue
+++ src/views/UserProfilePage.vue
@@ -1,19 +1,52 @@
 <template>
-  <div class="PageBody">
-    <Sidebar />
-    <UserProfile />
+  <div class="container">
+    <div class="sidebar">
+      <Sidebar />
+    </div>
+    <div class="notification-wrapper">
+      <Notification />
+    </div>
+    <div class="profile"> 
+      <UserProfile />
+    </div>
   </div>
 </template>
 
 <script>
 import Sidebar from "@/components/Sidebar.vue";
 import UserProfile from "@/components/UserProfile.vue";
+import Notification from "@/components/Notification.vue";
 
 export default {
   name: "UserProfilePage",
   components: {
     Sidebar,
     UserProfile,
+    Notification
   },
 };
 </script>
+<style scoped>
+.container {
+  display: flex;
+}
+
+.sidebar {
+  position: fixed;
+  top: 0;
+  width: 20%;
+  z-index: 1; /* Adjust the z-index value as needed */
+}
+
+.profile {
+  width: 80%;
+  margin-left: 20%; /* Adjust margin to accommodate the width of the sidebar */
+}
+
+.notification-wrapper {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 2; /* Set a higher z-index value for the notification wrapper to ensure it appears above the profile section */
+}
+</style>
